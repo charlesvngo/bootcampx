ed5289d8a009d9f12db66069afbec2e836f20193
Charles Ngo <charlesngo37@gmail.com>
2022-05-04 22:27:16 -0700

    add js files to query database

---
+++ bootcamp_app/students.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohort = '\'' + process.argv[2] + '%\'';
+const limit = process.argv[3] || 5;
+
+pool.query(`
+SELECT students.id AS id, students.name AS student_name, cohorts.name AS cohort_name
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE ${cohort}
+LIMIT ${limit}
+`)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.student_name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
